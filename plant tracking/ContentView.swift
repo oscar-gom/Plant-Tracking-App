1f1bf867bd60cae21d5d076a81049bd06a232f31
Oscar Gomez <oscargomezsedas@gmail.com>
2025-07-15 16:39:29 +0200

    Initial Commit

---
+++ plant tracking/ContentView.swift
@@ -0,0 +1,18 @@
+//
+//  ContentView.swift
+//  plant tracking
+//
+//  Created by Oscar Gomez on 15/7/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ plant tracking/plant_trackingApp.swift
@@ -0,0 +1,17 @@
+//
+//  plant_trackingApp.swift
+//  plant tracking
+//
+//  Created by Oscar Gomez on 15/7/25.
+//
+
+import SwiftUI
+
+@main
+struct plant_trackingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
