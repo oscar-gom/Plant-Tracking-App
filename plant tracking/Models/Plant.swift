941ec946677314c655cca61d3653313c831c6143
Oscar Gomez <oscargomezsedas@gmail.com>
2025-07-15 17:03:24 +0200

    feat: plant model created

---
+++ plant tracking/Models/Plant.swift
@@ -0,0 +1,30 @@
+//
+//  Plant.swift
+//  plant tracking
+//
+//  Created by Oscar Gomez on 15/7/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Plant {
+    var id: UUID
+    var name: String
+    var species: String?
+    var datePlanted: Date
+    var height: Double
+    var location: String?
+    var waterFrequency: String?
+    
+    init(name: String, species: String? = nil, datePlanted: Date, height: Double, location: String? = nil, waterFrequency: String? = nil) {
+        self.id = UUID()
+        self.name = name
+        self.species = species
+        self.datePlanted = datePlanted
+        self.height = height
+        self.location = location
+        self.waterFrequency = waterFrequency
+    }
+}
